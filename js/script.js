f0704cac3804cc2bcd2865dda74a9b7e1e75b921
youssef elbadry <youssefelbadry10@gmail.com>
2024-05-23 18:30:14 +0300

    egypttt

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+var btn1 = document.getElementById("btn1")
+function fun () {
+    alert(" this message is copied")
+}
+btn1.onclick = fun
+
+
+
+
+var btn2 = document.getElementById("btn2")
+function fun () {
+    alert(" this message is copied")
+}
+btn2.onclick = fun
+
+
+
+var btn3 = document.getElementById("btn3")
+function fun () {
+    alert(" this message is copied")
+}
+btn3.onclick = fun
+
+
